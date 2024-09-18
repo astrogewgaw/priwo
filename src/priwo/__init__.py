676b8c1c4f3ec3cbdba149ea76a98eadb13e3c53
Ujjwal Panda <ujjwalpanda97@gmail.com>
2024-09-18 19:13:48 +0530

    ✨ Add stubs.

--- src/priwo/__init__.py
+++ src/priwo/__init__.py
@@ -1,1 +1,23 @@
+from priwo._internals import (
+    readhdr,
+    readtim,
+    readfil,
+    readinf,
+    readdat,
+    readfft,
+    readpfd,
+    readpolycos,
+    readbestprof,
+)
 
+__all__ = [
+    "readhdr",
+    "readtim",
+    "readfil",
+    "readinf",
+    "readdat",
+    "readfft",
+    "readpfd",
+    "readpolycos",
+    "readbestprof",
+]
